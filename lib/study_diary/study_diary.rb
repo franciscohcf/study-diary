e69d6d08b577e485cd31da46776d8acf00175086
franciscohcf <franciscocavalcantefilho@gmail.com>
2021-11-22 18:01:15 -0300

    feat: study_diary add and all added

--- lib/study_diary/study_diary.rb
+++ lib/study_diary/study_diary.rb
@@ -1,7 +1,25 @@
 # frozen_string_literal: true
 
-require_relative 'category'
-require_relative 'study_item'
+class StudyDiary
+  require_relative 'category'
+  require_relative 'study_item'
+  attr_reader :contents
 
+  def initialize
+    @contents = {}
+  end
 
+  def add(category:, title:, description:)
+    study_item = StudyItem.new(title: title, description: description, category: category)
 
+    @contents[title] = study_item
+  end
+
+  def all
+    @contents.each do |i|
+      puts "Title: #{i[0]}"
+      puts "Description: #{i[1].description}"
+      puts
+    end
+  end
+end
